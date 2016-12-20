0a59401af1f5e4777b0ac3c90ca1619a61ba8e6c
SmithingChick <sharon.smith.metals@gmail.com>
2016-12-20 13:15:12 -0800

    blog content

---
+++ scripts/blogView.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var ourLocalData = [
+  {
+    title:       'Big Time',
+    category:    '90s',
+    performer:      'Peter Gabriel',
+    performerUrl:   'http://petergabriel.com/',
+    album:        'So',
+    publishedOn: '1986-01-05',
+    body:         '<p>Hi there! I/'m on my way, I/'m making it.  I/'ve got to make it show, yeah. So much larger than life. I/'m going to watch it growing. The place where I come from is a small town. They think so small, They use small words. But not me! I/'m smarter than that! I worked it out. I/'ve been stretching my mouth To let those big words come right out. I/'ve had enough, I/'m getting out To the city, the big big city. I/'ll be a big noise with all the big boys. There/'s so much stuff I will own And I will pray to a big god as I kneel in the big church. Big time! I/'m on my way-I/'m making it. Big time. I/'ve got to make it show yeah. Big time. So much larger than life! Big time. I/'m going to watch it growing. Big time! My parties all have big names And I greet them with the widest smile, Tell them how my life is one big adventure. And always they're amazed When I show them 'round my house, to my bed. I had it made like a mountain range With a snow-white pillow for my big fat head. And my heaven will be a big heaven And I will walk through the front door. Big time! I'm on my way-I'm making it. Big time! I've got to make it show-yeah! So much larger than life. I'm going to watch it growing.  Big time! My car is getting bigger. My house is getting bigger. My eyes are getting bigger And my mouth. My belly is getting bigger And my bank account. Look at my circumstance     And the bulge in my big big big big big big big</p>'
+  },
+  {
+    title:       'Even Gods Do',
+    category:    'singer-songwriter',
+    performer:      'Thea Gilmore',
+    performerUrl:   'http://www.theagilmore.net/',
+    album:        'Where's Neil When You Need Him?',
+    publishedOn: '2006-18-06',
+    body:         '<p>When she comes with a ghost of a smile And she roams through his eyes for a while, Days turn as empty ground somewhere. Bring the gods, line em up one by one. Turn the coin, sound the fife and the drum. Break it down until kingdom comes back here. Theres a rise, theres a fall Where the light hits the wall Spins a shadow. Theres a rise, theres a fall Where the light hits the wall Spins a... Cut him down from that old poplar tree. Break the cord while hes looking at me. Theres a cold wind blowing down through the meadow And hes the dark where the day has been bleeding. The ink this page has been needing You close that wood but it still casts a shadow. Theres a rise, theres a fall Where the light hits the wall Spins a shadow. Theres a rise, theres a fall Where the light hits the wall Spins a...Even gods do. Even gods. Theres a rise, theres a fall Where the light hits the wall Spins a shadow. Theres a rise, theres a fall Where the light hits the wall Spins a... </p>'
+  },
+  {
+    title:       'Dancing Barefoot',
+    category:    'punk poet',
+    performer:      'Patti Smith Group',
+    performerUrl:   'http://www.pattismith.net/intro.html',
+    album:        'Wave',
+    publishedOn: '1979-01-05',
+    body:         '<p>She is benediction, She is addicted to thee. She is the root connection, She is connecting with he. Here I go and I don't know why I flow so ceaselessly. Could it be he's taking over me? I'm dancing barefoot, Headin' for a spin. Some strange music draws me in. It makes me come up like some heroine. She is sublimation, She is the essence of thee. She is concentrating on He who is chosen by she. Here I go and I don't know why I spin so ceaselessly. Could it be he's taking over me? I'm dancing barefoot, Headin' for a spin. Some strange music draws me in. It makes me come up like some heroine. She is recreation, She intoxicated by thee. She has the slow sensation that He is levitating with she. Here I go and I don't know why I spin so ceaselessly. 'Til I lose my sense of gravity I'm dancing barefoot, Headin' for a spin. Some strange music draws me in. It makes me come up like some heroine. Oh God, I fell for you.</p>'
+  },
+  {
+    title:       '16 Shells From A Thirty-Ought-Six',
+    category:    'whiskey rock',
+    performer:    'Tom Waits',
+    performerUrl: 'http://www.tomwaits.com/',
+    album:        'Swordfishtrombones',
+    publishedOn: '1983-01-09',
+    body:         '<p>Plugged sixteen shells from a thirty-ought six And a black crow snuck through a hole in the sky. And I spent all my buttons on an old pack mule And I made me a ladder from a pawn shop marimba. I leaned it all up against a dandelion tree. Gonna whittle you into kindlin'. Sixteen shells from a thirty-ought six. Now I slept in the holler of a dry creek bed And I tore out the buckets from a red corvette. Lionel and Dave and the Butcher made three. Meet me by the knuckles of the skinnybone tree With the strings of a Washburn like a clothes line. Now me and that mule scrambled right through the hole. Gonna take a 38 hours long gone train I just Long gone train Just long gone train I just long Long</p>'
+  }
